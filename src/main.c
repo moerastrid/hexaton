e20622fbb51519f4b2589f81c58440782cc2a7eb
Astrid Geels <ageels@f1r3s7.codam.nl>
2022-03-07 17:41:25 +0100

    settings

--- src/main.c
+++ src/main.c
@@ -6,11 +6,35 @@
 /*   By: ageels <ageels@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/03/07 16:18:27 by ageels        #+#    #+#                 */
-/*   Updated: 2022/03/07 16:57:30 by ageels        ########   odam.nl         */
+/*   Updated: 2022/03/07 17:41:14 by ageels        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "hexathon.h"
+
+void	ft_setup_grid(void)
+{
+	char	**mygrid;
+	void	*start;
+	int		x;
+	int		y;
+
+	&start = mygrid + 10 * sizeof (char *);
+	mygrid = calloc(61 + 10 * sizeof (char *), sizeof(char));
+
+	y = 0;
+	while(y != 9)
+	{
+		x = 0;
+		while(x != 9)
+		{
+			if (y == 0)
+				
+			x++;
+		}
+		y++;
+	}
+}
 
 int	ft_total_size_grid(t_vari *data)
 {
@@ -31,25 +55,27 @@ int	ft_total_size_grid(t_vari *data)
 	return (length);
 }
 
-void	ft_make_grid(t_vari *data)
+void	ft_grid(t_vari *data)
 {
 	int	total_size;
 
 	total_size = ft_total_size_grid(data);
 	printf("%d", total_size);
+	if (total_size == 61)
+	{
+		ft_setup_grid();
+	}
 }
 
 int	main(void)
 {
 	t_vari	data;
 
+	//data = ft_read_settings();
 	data.shape = 0;
 	data.size = 5;
 	data.colors = 2;
 	data.figure = 0;
-	ft_make_grid(&data);
+	ft_grid(&data);
 	return (0);
 }
-
-
-
