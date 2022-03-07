101a624946662ee68858a425f1167a126304a4c5
Astrid Geels <ageels@f1r3s7.codam.nl>
2022-03-07 16:58:06 +0100

    formule van bas

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: ageels <ageels@student.codam.nl>             +#+                      #
 #                                                    +#+                       #
 #    Created: 2022/03/07 16:22:41 by ageels        #+#    #+#                  #
-#    Updated: 2022/03/07 16:33:07 by ageels        ########   odam.nl          #
+#    Updated: 2022/03/07 16:54:52 by ageels        ########   odam.nl          #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,14 +14,14 @@ NAME	= hexathon
 CFLAGS	= -w -Wunreachable-code -Ofast
 LIBMLX	= ./lib/MLX42
 
-HEADERS	= -I ./include -I $(LIBMLX)/include ./src
+HEADERS	= -I ./include -I $(LIBMLX)/include
 LIBS	= -lglfw -L /Users/$(USER)/.brew/opt/glfw/lib/ $(LIBMLX)/libmlx42.a
 SRCS	= $(shell find ./src -iname "*.c")
 OBJS	= ${SRCS:.c=.o}
 
 BOLD	= \033[1m
 BLACK	= \033[30;1m
-RED	= \033[31;1m
+RED		= \033[31;1m
 GREEN	= \033[32;1m
 YELLOW	= \033[33;1m
 BLUE	= \033[34;1m
--- src/main.c
+++ src/main.c
@@ -6,15 +6,37 @@
 /*   By: ageels <ageels@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/03/07 16:18:27 by ageels        #+#    #+#                 */
-/*   Updated: 2022/03/07 16:41:34 by ageels        ########   odam.nl         */
+/*   Updated: 2022/03/07 16:57:30 by ageels        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "headerfile.h"
+#include "hexathon.h"
+
+int	ft_total_size_grid(t_vari *data)
+{
+	int	length;
+	int	length_2;
+	int	count;
+
+	length = data->size;
+	length_2 = length;
+	count = 1;
+	while (count < length_2 - 1)
+	{
+		length += length_2 + count;
+		count++;
+	}
+	length *= 2;
+	length += length_2 * 2 - 1;
+	return (length);
+}
 
 void	ft_make_grid(t_vari *data)
 {
-	
+	int	total_size;
+
+	total_size = ft_total_size_grid(data);
+	printf("%d", total_size);
 }
 
 int	main(void)
