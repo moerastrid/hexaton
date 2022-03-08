1be9d87250fc4c6d985b58873be357d701ea02e6
Robert Beens <rbeens@f0r4s8.codam.nl>
2022-03-08 21:09:39 +0100

    robert zijn dingetjes

---
+++ robert/bot1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int	 main(void)
+{
+	int num;
+	int filenum = open("test.txt", O_WRONLY | O_CREAT);
+
+	write(filenum, "test", 4);
+	while(1)
+	{
+		scanf("%*s %d", &num);
+		write(filenum, "test", 4);
+		num += 1;
+		printf("%d\n", num);
+		fflush(stdout);
+	}
+	return(0);
+}
+
+
+
