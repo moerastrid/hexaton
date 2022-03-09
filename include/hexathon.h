e0bbac6f27e8a983a8ce9e7de07c9acbef0fb142
Maarten Van der vlugt <mvan-der@f1r2s7.codam.nl>
2022-03-09 17:05:04 +0100

    bag of colors wrking with picking function

--- include/hexathon.h
+++ include/hexathon.h
@@ -6,7 +6,7 @@
 /*   By: ageels <ageels@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/03/07 16:38:46 by ageels        #+#    #+#                 */
-/*   Updated: 2022/03/09 13:34:33 by mvan-der      ########   odam.nl         */
+/*   Updated: 2022/03/09 17:03:17 by mvan-der      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,8 @@
 typedef struct s_player
 {
 	char	*tile_set;
-	char	*tile1;
-	char	*tile2;
+	char	tile1;
+	char	tile2;
 }	t_player;
 
 typedef struct s_vari
@@ -36,12 +36,15 @@ typedef struct s_vari
 	int		shape;
 	int		figure;
 	char	**grid;
-	char	max_tiles;
+	int		max_tiles;
+	int		max_colors;
 	t_player	*player1;
 	t_player	*player2;
 	mlx_t		*mlx;
 	mlx_image_t	*img;
 }		t_vari;
+
+
 char		**ft_setup_grid(int side);
 //int			ft_total_size_grid(t_vari *data);
 char		**ft_grid(t_vari *data);
@@ -50,7 +53,8 @@ void		ft_draw(t_vari *data, mlx_image_t *img);
 void		ft_draw_hexagon(int32_t x, int32_t y, mlx_image_t *img, char c);
 char		*random_tile_grabber1(t_vari *data);
 char		random_tile_grabber2(t_vari *data);
-void		ft_create_bag_of_colors(t_vari *data);
-int			ft_choose_color(t_vari *data, int current_player);
+// void		ft_create_bag_of_colors(t_vari *data);
+t_player	*ft_create_bag_of_colors(t_vari *data, char *color1, char *color2);
+int			ft_picking_tiles(t_vari *data, int current_player);
 mlx_keyfunc	ft_keypress(mlx_key_data_t keydata, void *invar);
 #endif
--- src/bag_of_colors.c
+++ src/bag_of_colors.c
@@ -6,13 +6,43 @@
 /*   By: mvan-der <mvan-der@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/03/09 10:45:38 by mvan-der      #+#    #+#                 */
-/*   Updated: 2022/03/09 13:32:07 by mvan-der      ########   odam.nl         */
+/*   Updated: 2022/03/09 17:01:14 by mvan-der      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 #include <stdio.h>
 #include "../include/hexathon.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*newstr;
+	size_t	i;
+	size_t	j;
+
+	if (!s1 || !s2)
+		return (0);
+	newstr = calloc(sizeof(char), strlen(s1) + strlen(s2) + 1);
+	if (!newstr)
+		return (0);
+	i = 0;
+	j = 0;
+	while (s1[i] != '\0')
+	{
+		newstr[j] = s1[i];
+		i++;
+		j++;
+	}
+	i = 0;
+	while (s2[i] != '\0')
+	{
+		newstr[j] = s2[i];
+		i++;
+		j++;
+	}
+	free(s1);
+	return (newstr);
+}
 
 char	*ft_strdup(const char *s)
 {
@@ -32,11 +62,11 @@ char	*ft_strdup(const char *s)
 	return (dest);
 }
 
-int 	ft_choose_color(t_vari *data, int current_player)
+int 	ft_picking_tiles(t_vari *data, int current_player)
 {
 	if (current_player == 1)
 	{
-		int upper = 12;
+		int upper = 12; // afhankelijk van size maken
 		int lower = 0;
 
 		size_t len = strlen(data->player1->tile_set);
@@ -64,7 +94,7 @@ int 	ft_choose_color(t_vari *data, int current_player)
 	if (current_player == 2)
 	{
 	
-		int upper = 12;
+		int upper = 12; // afhankelijk van size maken
 		int lower = 0;
 
 		size_t len = strlen(data->player2->tile_set);
@@ -96,18 +126,65 @@ int 	ft_choose_color(t_vari *data, int current_player)
 	}
 }
 
-
-void	ft_create_bag_of_colors(t_vari *data)
+void	ft_create_bags_of_colors(t_vari *data)
 {
-	t_player	*player1;
-	t_player	*player2;
-
-	player1 = malloc(sizeof(char) * 1000);
-	player2 = malloc(sizeof(char) * 1000);
-	
-	player1->tile_set = ft_strdup("bbbbbbcccccc"); //p = purple b = blue
-	player2->tile_set = ft_strdup("ddddddeeeeee"); //r = red o = orange
-	data->player1 = player1;
-	data->player2 = player2;
+	data->player1 = ft_create_bag_of_colors(data, "b", "c");
+	data->player2 = ft_create_bag_of_colors(data, "d", "e");
 }
 
+// void	ft_create_bag_of_colors(t_vari *data)
+// {
+// 	t_player	*player1;
+// 	t_player	*player2;
+// 	int i;
+
+// 	player1 = malloc(sizeof(char) * 1000);
+// 	player2 = malloc(sizeof(char) * 1000);
+
+// 	// tile_set contains max_colors, equal distribution of 2 colors.
+// 	for (i = 0; i < (data->max_colors / 2); i++)
+// 	{
+// 		if (i == 0)
+// 			player1->tile_set = ft_strdup("b");
+// 		else
+// 			player1->tile_set = ft_strjoin(player1->tile_set, "b");
+// 	}
+// 	for (i = 0; i < (data->max_colors / 2); i++)
+// 		player1->tile_set = ft_strjoin(player1->tile_set, "c");
+
+// 	for (i = 0; i < (data->max_colors / 2); i++)
+// 	{
+// 		if (i == 0)
+// 			player2->tile_set = ft_strdup("b");
+// 		else
+// 			player2->tile_set = ft_strjoin(player2->tile_set, "d");
+// 	}
+// 	for (i = 0; i < (data->max_colors / 2); i++)
+// 		player2->tile_set = ft_strjoin(player2->tile_set, "e");
+	
+// 	// player1->tile_set = ft_strdup("bbbbbbcccccc"); //p = purple b = blue
+// 	// player2->tile_set = ft_strdup("ddddddeeeeee"); //r = red o = orange
+// 	data->player1 = player1;
+// 	data->player2 = player2;
+// 	printf("P1 tile set?: %s\n", data->player1->tile_set);
+// }
+
+t_player	*ft_create_bag_of_colors(t_vari *data, char *color1, char *color2)
+{
+	t_player	*player;
+	int i;
+
+	player = malloc(sizeof(t_player));
+
+	// tile_set contains max_colors, equal distribution of 2 colors.
+	for (i = 0; i < (data->max_colors / 2); i++)
+	{
+		if (i == 0)
+			player->tile_set = ft_strdup(color1);
+		else
+			player->tile_set = ft_strjoin(player->tile_set, color1);
+	}
+	for (i = 0; i < (data->max_colors / 2); i++)
+		player->tile_set = ft_strjoin(player->tile_set, color2);
+	return (player);
+}
